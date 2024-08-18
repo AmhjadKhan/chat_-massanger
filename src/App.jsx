967b156dc262a9b63f6b37d9090d3850a1339bc4
Amhjadkhan <amjadpogramer@gmail.com>
2024-08-19 00:55:42 +0400

    this is pages folder create commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
-
+import { Routes,Route} from "react-router-dom"
+import Home from "./pages/Home"
+import Login from "./pages/Login"
+import Signup from "./pages/Signup"
 
 const App = () => {
   return (
     <div>
-      <h1 className="">Amjad Hossain </h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
     </div>
   )
 }
---
+++ src/pages/Signup.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Signup = () => {
+  return (
+    <div>
+      <h1>sign uo</h1>
+    </div>
+  )
+}
+
+export default Signup
