0c08b62e5f4de336251f1ca74c9106695d3f285d
Amhjadkhan <amjadpogramer@gmail.com>
2024-08-19 16:35:53 +0400

    this is singup page gate value in console

--- src/components/massages/MessageInput.jsx
+++ src/components/massages/MessageInput.jsx
@@ -10,7 +10,6 @@ const MessageInput = () => {
           className="border text-sm rounded-lg block w-full p-2.5 bg-gray-700 border-gray-600 text-white"
           placeholder="Enter your text..."
         />
-
         <button
           type="submit"
           className="absolute inset-y-0 end-0 flex items-center pe-3"
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,8 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import GenderCheckbox from "../components/GenderCheckbox";
 
 const Signup = () => {
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+    gender: ""
+  });
+
+  // Handle gender selection
+  const handleCheckboxChange = (gender) => {
+    setFormData({ ...formData, gender });
+  };
+
+  // Handle form submission
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log(formData); 
+  };
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen px-4 sm:px-6 lg:px-8">
       <div className="w-full max-w-md p-6 rounded-lg shadow-md bg-black">
@@ -11,7 +30,7 @@ const Signup = () => {
           <span className="text-blue-500"> Chat Messenger</span>
         </h1>
 
-        <form>
+        <form onSubmit={handleSubmit}>
           <div>
             <label className="block text-sm font-medium text-white mt-4">
               Username
@@ -20,6 +39,8 @@ const Signup = () => {
               type="text"
               placeholder="Enter Username"
               className="w-full px-3 py-2 mt-1 text-white bg-gray-800 border border-gray-600 rounded-md focus:outline-none focus:ring focus:ring-blue-500 focus:border-blue-500"
+              value={formData.username}
+              onChange={(e) => setFormData({ ...formData, username: e.target.value })}
             />
           </div>
 
@@ -31,6 +52,8 @@ const Signup = () => {
               type="email"
               placeholder="example@gmail.com"
               className="w-full px-3 py-2 mt-1 text-white bg-gray-800 border border-gray-600 rounded-md focus:outline-none focus:ring focus:ring-blue-500 focus:border-blue-500"
+              value={formData.email}
+              onChange={(e) => setFormData({ ...formData, email: e.target.value })}
             />
           </div>
 
@@ -42,6 +65,8 @@ const Signup = () => {
               type="password"
               placeholder="Enter Password"
               className="w-full px-3 py-2 mt-1 text-white bg-gray-800 border border-gray-600 rounded-md focus:outline-none focus:ring focus:ring-blue-500 focus:border-blue-500"
+              value={formData.password}
+              onChange={(e) => setFormData({ ...formData, password: e.target.value })}
             />
           </div>
 
@@ -53,9 +78,13 @@ const Signup = () => {
               type="password"
               placeholder="Confirm Password"
               className="w-full px-3 py-2 mt-1 text-white bg-gray-800 border border-gray-600 rounded-md focus:outline-none focus:ring focus:ring-blue-500 focus:border-blue-500"
+              value={formData.confirmPassword}
+              onChange={(e) => setFormData({ ...formData, confirmPassword: e.target.value })}
             />
           </div>
-          <GenderCheckbox />
+
+          <GenderCheckbox onCheckboxChange={handleCheckboxChange} selectedGender={formData.gender} />
+
           <Link
             to="/login"
             className="block text-sm font-medium text-blue-500 hover:underline mt-4"
