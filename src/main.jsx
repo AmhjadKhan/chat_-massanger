6dc30b2be3d635e637665328c1f082e5d02e62eb
Amhjadkhan <amjadpogramer@gmail.com>
2024-08-21 17:10:26 +0400

    this is socate io clint contex commit

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,15 @@ import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { AuthContextProvider } from "./contex/AuthContext.jsx";
+import { SocketContextProvider } from "./contex/SocketContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <AuthContextProvider>
       <BrowserRouter>
-        <App />
+        <SocketContextProvider>
+          <App />
+        </SocketContextProvider>
       </BrowserRouter>
     </AuthContextProvider>
   </StrictMode>
